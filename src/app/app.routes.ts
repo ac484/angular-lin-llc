34425865640641b0816b5621985d48a207b3d70e
𓂺 <ac7x7x@gmail.com>
2025-07-08 01:45:24 +0800

    重構路由設定，將元件改為模組載入，刪除不必要的元件檔案，提升程式碼結構與可讀性，符合極簡主義開發原則。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,28 @@
 import { Routes } from '@angular/router';
-import { WorkspacesComponent } from './workspaces/workspaces.component';
 
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+    loadComponent: () => import('./login/login.module').then(m => m.LoginComponent)
   },
   {
     path: '',
-    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./home/home.module').then(m => m.HomeComponent)
   },
   {
     path: 'account',
-    loadComponent: () => import('./account/account.component').then(m => m.AccountComponent)
+    loadComponent: () => import('./account/account.module').then(m => m.AccountComponent)
   },
   {
     path: 'workspace',
     loadChildren: () => import('./workspace/workspace.module').then(m => m.WorkspaceModule)
   },
   {
+    path: 'workspaces',
+    loadComponent: () => import('./workspaces/workspaces.module').then(m => m.WorkspacesModule)
+  },
+  {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   },
-  { path: 'workspaces', component: WorkspacesComponent },
 ];
