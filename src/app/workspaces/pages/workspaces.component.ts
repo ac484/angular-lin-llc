f480f75cbe6299540e25d5c231f3c10f361d15ef
𓂺 <ac7x7x@gmail.com>
2025-07-08 03:30:54 +0800

    更新 package.json 和 pnpm-lock.yaml，將 @angular/fire、@primeng/themes 和 primeng 版本統一至 19.0.0，刪除不必要的帳號模組及相關元件，提升程式碼結構與可讀性，符合極簡主義開發原則。

--- src/app/workspaces/pages/workspaces.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'app-workspaces',
-  templateUrl: './workspaces.component.html',
-  styleUrls: ['./workspaces.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class WorkspacesComponent {}
