3416b10765909155948571f0451856a2bdd4ca30
𓂺 <ac7x7x@gmail.com>
2025-07-08 12:17:47 +0800

    刪除樹狀結構元件及相關樣式，簡化工作區元件結構，提升可讀性，符合極簡主義開發原則。

--- src/app/pages/workspace/workspace.component.ts
+++ src/app/pages/workspace/workspace.component.ts
@@ -1,25 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { TreeService } from '../../services/tree.service';
-import { PrimeNgModules } from '../../shared/modules/prime-ng.module';
-import { AsyncPipe, NgIf } from '@angular/common';
-import { WorkspaceSidenavComponent } from '../../layouts/main-layout/workspace-sidenav/workspace-sidenav.component';
 
 @Component({
   selector: 'app-workspace',
   templateUrl: './workspace.component.html',
   styleUrls: ['./workspace.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  standalone: true,
-  imports: [
-    ...PrimeNgModules,
-    WorkspaceSidenavComponent,
-    AsyncPipe,
-    NgIf
-  ]
+  standalone: true
 })
-export class WorkspaceComponent {
-  treeNodes$;
-  constructor(private treeService: TreeService) {
-    this.treeNodes$ = this.treeService.getTreeNodes();
-  }
-}
+export class WorkspaceComponent {}
