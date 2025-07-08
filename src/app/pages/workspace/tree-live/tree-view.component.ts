2b62166e707a6304797269fdd4fe8724916e302b
𓂺 <ac7x7x@gmail.com>
2025-07-08 15:18:55 +0800

    刪除不再使用的樹狀結構樣式檔案，並將樹狀結構元件的樣式更新為 SCSS 格式，以符合極簡主義開發原則，提升專案整潔性。

--- src/app/pages/workspace/tree-live/tree-view.component.ts
+++ src/app/pages/workspace/tree-live/tree-view.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
-  styleUrls: ['./tree-view.component.css'],
+  styleUrls: ['./tree-view.component.scss'],
 })
 export class TreeViewComponent {
   @Input() tree: any;
